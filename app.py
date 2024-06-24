21ae90daf99a34b6eb5bdfe35feb3d15de9870f4
asyanest <asyanest@gmail.com>
2024-06-24 10:41:03 +0300

    modified:   app.py

--- app.py
+++ app.py
@@ -22,19 +22,17 @@ actual_range=list(range(price_range[0],price_range[1]+1))
 last_years = st.checkbox('Newer than 2010 only')
 
 if last_years:
-    filtered_data=data[data.price.isin[actual_range]]
-    filtered_data=filtered_data[data.model_year >= 2010]
+    filtered_data=data[data.price.isin(actual_range) & (data.model_year >= 2010)]
 else:
     filtered_data=data[data.price.isin(actual_range)]
     
 st.write('Here are your options with a split by price and year')
-
-fig = px.scatter(filtered.data, x="price", y="model_year")
+fig = px.scatter(filtered_data, x="price", y="model_year")
 st.plotly_chart(fig)
 
 st.write('Condition VS Year')
-fig2 = px.histogram(filtered.data, x='model_year',color='condition')
+fig2 = px.histogram(filtered_data, x='model_year',color='condition')
 st.write(fig2)
 
 st.write('Here is the list of recomended cars')
-st.dataframe(filtered.data.sample(20))
+st.dataframe(filtered_data.sample(20))
